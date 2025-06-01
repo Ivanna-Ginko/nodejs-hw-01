55a3c3bc6765b0ac7e03d26b579eb0c8edad0a93
unknown <ivannabelichenko@gmail.com>
2025-06-01 22:00:34 +0300

    2

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -2,7 +2,9 @@ import { getAllContacts } from "./getAllContacts.js";
 
 export const countContacts = async () => {
     const array = await getAllContacts();
+    //console.log(array.length)
     return array.length;
+
 };
 
-//console.log(await countContacts());
+console.log(await countContacts());
